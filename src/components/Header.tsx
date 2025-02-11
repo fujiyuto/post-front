cc0d70d1e98415b956dab6930d4c6c16ea4e900e
DESKTOP-GD87MM6\ayase <ayase.kasagi@t8s.co.jp>
2025-02-11 18:15:02 +0900

    ヘッダー非同期処理の実装

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,9 +9,12 @@ import { SearchGenre } from './Search/Search'
 import { SearchGenreModal } from './Modal/SearchGenreModal'
 import { signOut, useSession } from 'next-auth/react'
 import { Button } from '@mui/base'
+import { getServerSession } from "next-auth";
+// import { signOut, Session } from "next-auth/react";
 
-export const GenreContext = createContext<() => void>(() => {})
-export const LocateContext = createContext<() => void>(() => {})
+
+export const GenreContext = createContext<() => void>(() => { })
+export const LocateContext = createContext<() => void>(() => { })
 
 export const Header = () => {
     const [openGenre, setOpenGenre] = useState<boolean>(false)
@@ -21,7 +24,7 @@ export const Header = () => {
     const [openLocate, setOpenLocate] = useState<boolean>(false)
     const handleLocateOpen = () => setOpenLocate(true)
     const handleLocateClose = () => setOpenLocate(false)
-    
+
     const [openKeyword, setOpenkeyword] = useState<boolean>(false)
     const handleKeywordOpen = () => setOpenkeyword(true)
     const handleKeywordClose = () => setOpenkeyword(false)
@@ -29,10 +32,10 @@ export const Header = () => {
     const { data: session, status } = useSession()
 
     const logoutAction = async (): Promise<void> => {
-        const data = await signOut({redirect: false, callbackUrl: 'http://localhost:3000/'})
+        const data = await signOut({ redirect: false, callbackUrl: 'http://localhost:3000/' })
 
     }
-    
+
     return (
         <header
             className='h-16 flex justify-between items-center px-7 text-theme border-b-4'
@@ -43,33 +46,28 @@ export const Header = () => {
             <ul className='flex text-lg gap-8'>
                 <li className='hover:cursor-pointer hover:opacity-80' onClick={handleGenreOpen}>ジャンルで探す</li>
                 <li>地域で探す</li>
-                {
-                    status === 'authenticated' && (
-                        <>
-                            <li>
-                                <Link href='/mypage'>マイページ</Link>
-                            </li>
-                            <li>
-                                <form action={logoutAction}>
-                                    <Button type='submit'>ログアウト</Button>
-                                </form>
-                            </li>
-                        </>
-                    )
-                }
-                {
-                    status === 'unauthenticated' && (
-                        <>
-                            <li>
-                                <Link href='/users/create'>新規会員登録</Link>
-                            </li>
-                            <li>
-                                <Link href='/users/login'>ログイン</Link>
-                            </li>
-                        </>
-                    )
-                }
-                
+                {session ? (
+                    <>
+                        <li>
+                            <Link href="/mypage">マイページ</Link>
+                        </li>
+                        <li>
+                            <form action={logoutAction}>
+                                <Button type="submit">ログアウト</Button>
+                            </form>
+                        </li>
+                    </>
+                ) : (
+                    <>
+                        <li>
+                            <Link href="/users/create">新規会員登録</Link>
+                        </li>
+                        <li>
+                            <Link href="/users/login">ログイン</Link>
+                        </li>
+                    </>
+                )}
+
             </ul>
             <GenreContext.Provider value={handleGenreClose}>
                 <SearchGenreModal
@@ -83,7 +81,7 @@ export const Header = () => {
             >
                 <Fade in={openLocate}>
                     <div>
-                        
+
                     </div>
                 </Fade>
             </Modal>
@@ -94,7 +92,7 @@ export const Header = () => {
             >
                 <Fade in={openKeyword}>
                     <div>
-                        
+
                     </div>
                 </Fade>
             </Modal>
@@ -106,13 +104,13 @@ interface BackdropProps {
     open: boolean
     onClick: () => void
 }
-const Backdrop = React.forwardRef<HTMLDivElement,BackdropProps>((props, ref) => {
+const Backdrop = React.forwardRef<HTMLDivElement, BackdropProps>((props, ref) => {
     const { open, onClick } = props
     return (
-       <div 
-        className={clsx({ 'base-Backdrop-open': open }, '-z-10 fixed inset-0 bg-black opacity-30')}
-        ref={ref}
-        onClick={onClick}
+        <div
+            className={clsx({ 'base-Backdrop-open': open }, '-z-10 fixed inset-0 bg-black opacity-30')}
+            ref={ref}
+            onClick={onClick}
         />
     )
 })
